6afb1ef5cb16b3ae32a5ad0bab025572dc8c4721
Virus Rush Theater <virusrushtheater@gmail.com>
2016-11-16 10:55:35 -0300

    Variant JSON parser capabilities tested

--- Test04/test04.cpp
+++ Test04/test04.cpp
@@ -1,34 +1,35 @@
 #include "whimsycore.h"
 #include <iostream>
+#include <stdio.h>
+
+#define JSON_PATH   "variant.json"
 
 using namespace whimsycore;
 
 int main(int argc, char** argv)
 {
-    Variant hst, hou;
-    hst["webo"][15000]["lel"] = "pedro";
+    FILE*   fh;
+    char*   fbuffer;
+    Variant json;
+    size_t  fsize;
 
-    hst["mobius"] = "The Architect";
-    hst["kek"] = "Json";
-    hst["kek"]["1"] = "1";
-    hst["kek"]["2"] = "2";
-    hst["kek"]["3"]["a"]["i"] = "3ai";
-    hst["kek"]["3"]["a"]["ii"] = "3aii";
-    hst["kek"]["3"]["a"]["iii"] = "3aiii";
-    hst["kek"]["3"]["b"] = Note("C-2");
-    hst["kek"]["3"]["c"] = Note("C#2");
-    hst["kek"]["3"]["d"][0] = "the host 1";
-    hst["kek"]["3"]["d"][1] = "the host 2";
-    hst["kek"]["3"]["d"][2] = 5.5;
-    hst["kek"]["3"]["d"][3] = "the host 4";
+    fh = fopen(JSON_PATH, "r");
+    if(!fh)
+        return 1;
 
-    hst = 3.14;
-    hst = 1592;
+    fseek(fh, 0, SEEK_END);
+    fsize = ftell(fh);
+    rewind(fh);
 
-    hou.parse("{\"uno\":1, \"dos\":2, \"tres\":3}");
+    fbuffer = (char*) malloc(fsize + 1);
 
-    std::cout << hou << std::endl;
-    std::cout << hou["uno"] << std::endl;
+    fread(fbuffer, sizeof(char), fsize + 1, fh);
+    fclose(fh);
+
+    json.parse(fbuffer);
+    std::cout << json.toJSONPretty() << std::endl;
+
+    free(fbuffer);
 
     return 0;
 }
